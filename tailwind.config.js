271041f8b40c31f6ca5fabc431414d6095e5da28
yeremiaChris <yeremia997@gmail.com>
2021-08-01 14:43:01 +0700

    Configuration Tailwindcss Update index.js (not finish yet)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      spacing: {
+        "50px": "50px",
+        "100px": "100px",
+      },
+    },
+  },
+  plugins: [],
+};
