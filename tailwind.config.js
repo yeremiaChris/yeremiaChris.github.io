db1a0ed958b9ae0ddcc65d5c89d0222ed00c4029
yeremiaChris <yeremia997@gmail.com>
2022-01-25 08:31:32 +0700

    fix ui of portofolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: false,
+    content: ["./*.html"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
-      spacing: {
-        "50px": "50px",
-        "100px": "100px",
+      extend: {
+        spacing: {
+          "50px": "50px",
+          "100px": "100px",
+          "1080px": "1080px",
+        },
       },
     },
   },
+  variants: {},
   plugins: [],
 };
